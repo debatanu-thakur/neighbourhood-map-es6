a075f46cacd1072870290316ff287b17062940ea
Debatanu Thakur <Debatanu_Thakur@ad.infosys.com>
2016-11-14 18:03:32 +0530

    Add search location for neighborhood

--- js/main.js
+++ js/main.js
@@ -1,7 +1,8 @@
 (function($){
   $(function(){
-
+    //Event initializations
     $('.button-collapse').sideNav();
+    $('.modal').modal();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
