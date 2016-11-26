3a5d693ca81d73ea0d8b283889e589830ec0a97f
Debatanu Thakur <Debatanu_Thakur@ad.infosys.com>
2016-11-27 01:48:51 +0530

    feat: Search Add animation of search bar on focus

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@
     //Event initializations
     $('.button-collapse').sideNav();
     $('.modal').modal();
-
+    $('input#search').focus(function() { $(this).parent().addClass('focused'); });
+    $('input#search').blur(function() {
+      if (!$(this).val()) {
+        $(this).parent().removeClass('focused');
+      }
+    });
   }); // end of document ready
 })(jQuery); // end of jQuery name space
